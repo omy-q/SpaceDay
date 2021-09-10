40efa062151bb380c4db5895d5b437fd683e82ba
omy-q <olya_aparina@mail.ru>
2021-09-10 23:09:57 +0300

    Создание основного экрана: 1. Создание CustomView для квадратизации изображения 2. Использование TextInputLayout для красивого отображения EditText для поиска слов в Википедии 3. Создание нижнего action bar 4. Создание bottom sheet для отображения описания к фото

--- app/src/main/java/com/example/spaceday/superview/view/MainFragment.kt
+++ app/src/main/java/com/example/spaceday/superview/view/MainFragment.kt
@@ -1,15 +1,16 @@
 package com.example.spaceday.superview.view
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.spaceday.R
 import com.example.spaceday.databinding.MainFragmentBinding
 import com.example.spaceday.superview.viewmodel.AppState
 import com.example.spaceday.superview.viewmodel.MainViewModel
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 
 class MainFragment :Fragment() {
 
@@ -19,6 +20,8 @@ class MainFragment :Fragment() {
         ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<ConstraintLayout>
+
     companion object{
         fun newInstance() = MainFragment()
     }
@@ -27,10 +30,37 @@ class MainFragment :Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = MainFragmentBinding.inflate(inflater, container,false)
+        setBottomAppBar()
         return binding.root
+    }
+
+    private fun setBottomAppBar(){
+        (context as MainActivity).setSupportActionBar(binding.bottomAppBar)
+        setHasOptionsMenu(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.menu_bottom_bar, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.app_bar_load -> {
+                Toast.makeText(context, "Download", Toast.LENGTH_SHORT).show()
+            }
+            R.id.app_bar_settings -> {
+                Toast.makeText(context, "Settings", Toast.LENGTH_SHORT).show()
+            }
+            android.R.id.home -> {
+                BottomNavigationDrawerFragment.newInstance()
+                    .show(requireActivity().supportFragmentManager, "")
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onDestroyView() {
@@ -44,6 +74,17 @@ class MainFragment :Fragment() {
             getLiveData().observe(viewLifecycleOwner, {renderData(it)})
             getData()
         }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initBottomSheet(binding.mainContent.bottomSheetLayout.bottomSheetContainer)
+    }
+
+    private fun initBottomSheet(bottomSheet: ConstraintLayout) {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+        bottomSheetBehavior.state = BottomSheetBehavior.STATE_COLLAPSED
     }
 
     private fun renderData(appState : AppState){
