920b4e2a7bed14ce419cc92d74ac7233ecd31ecd
omy-q <olya_aparina@mail.ru>
2021-09-17 19:37:18 +0300

    Использование библиотеки CircleIndicator для добавления анимации в индикатор промотки

--- app/src/main/java/com/example/spaceday/superview/view/planet/PlanetActivity.kt
+++ app/src/main/java/com/example/spaceday/superview/view/planet/PlanetActivity.kt
@@ -2,7 +2,9 @@ package com.example.spaceday.superview.view.planet
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.spaceday.R
 import com.example.spaceday.databinding.ActivityPlanetBinding
+import me.relex.circleindicator.CircleIndicator3
 
 class PlanetActivity : AppCompatActivity() {
 
@@ -10,7 +12,8 @@ class PlanetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlanetBinding.inflate(layoutInflater)
-        setContentView(binding.root)
         binding.viewPager.adapter = ViewPlanetPagerAdapter(this)
+        binding.indicator.setViewPager(binding.viewPager)
+        setContentView(binding.root)
     }
 }
