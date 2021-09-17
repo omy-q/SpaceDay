a4b5f52ad80130a4156db2de7c050925d8d9623c
omy-q <olya_aparina@mail.ru>
2021-09-17 23:40:16 +0300

    Создание вкладок (tabs) для экрана с Землей и Марсов

--- app/src/main/java/com/example/spaceday/superview/view/planet/PlanetActivity.kt
+++ app/src/main/java/com/example/spaceday/superview/view/planet/PlanetActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.spaceday.R
 import com.example.spaceday.databinding.ActivityPlanetBinding
+import com.google.android.material.tabs.TabLayoutMediator
 import me.relex.circleindicator.CircleIndicator3
 
 class PlanetActivity : AppCompatActivity() {
@@ -12,8 +13,17 @@ class PlanetActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPlanetBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         binding.viewPager.adapter = ViewPlanetPagerAdapter(this)
         binding.indicator.setViewPager(binding.viewPager)
-        setContentView(binding.root)
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = "OBJECT ${(position + 1)}"
+        }.attach()
+
+        binding.tabLayout.getTabAt(0)?.customView =
+            layoutInflater.inflate(R.layout.tablayout_earth,null)
+        binding.tabLayout.getTabAt(1)?.customView =
+            layoutInflater.inflate(R.layout.tablayout_mars,null)
     }
 }
