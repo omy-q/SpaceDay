488f0001526c38e6a33cf6d4a744490401842ad5
omy-q <olya_aparina@mail.ru>
2021-09-21 18:58:50 +0300

    добавление аннотации Parcelize

--- app/src/main/java/com/example/spaceday/supermodel/remote/NASAData.kt
+++ app/src/main/java/com/example/spaceday/supermodel/remote/NASAData.kt
@@ -1,7 +1,10 @@
 package com.example.spaceday.supermodel.remote
 
+import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
+import kotlinx.android.parcel.Parcelize
 
+@Parcelize
 data class NASAData (
     @field:SerializedName("date") val date: String?,
     @field:SerializedName("explanation") val explanation: String?,
@@ -10,4 +13,4 @@ data class NASAData (
     @field:SerializedName("service_version") val service_version: String?,
     @field:SerializedName("title") val title: String?,
     @field:SerializedName("url") val url: String?,
-)
+) : Parcelable
